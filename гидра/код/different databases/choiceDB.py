f8a67ca840823f3eb51623d10fe4bf07dbc7f5c4
Kiranuell <60107882+Kiranuell@users.noreply.github.com>
2021-04-27 17:42:05 +0400

    Add files via upload

--- гидра/код/different databases/choiceDB.py
+++ гидра/код/different databases/choiceDB.py
@@ -7,21 +7,26 @@ app = Flask(__name__)
 @app.route('/', methods=['post', 'get'])
 def login():
     message = ''
+    # если отправили данные
     if request.method == 'POST':
+        # переменные для подключения
         host = request.form.get('host')  # запрос к данным формы
         database = request.form.get('database')
         user = request.form.get('user')
         password = request.form.get('password')
-        print(host, database, user, password)
+        # проверка на наличие всех переменных
         if host and database and user and password:
+            # попытка подключиться
             try:
                 connection = pymysql.connect(host=host, user=user, passwd=password, database=database)
+                # успех
                 if connection:
-                    print("ok")
                     return render_template('success.html', message=message)
+            # ошибка
             except:
                 return render_template('fail.html', message=message)
 
+    # страница с формой
     return render_template('data.html', message=message)
 
 
